357d287c61d2cb769e0422d6f65da9c574084b04
Ola <tundock007@gmail.com>
2017-07-08 00:39:25 -0700

    Initial

---
+++ src/com/Olatunde/Main.java
@@ -0,0 +1,14 @@
+package com.Olatunde;
+
+import java.util.Scanner;
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+    }
+
+    
+
+
+}
